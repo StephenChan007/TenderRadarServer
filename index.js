38d801dee8bb476830c5c15105f2500d98e16e47
chengpeng6 <chengpeng6@xiaomi.com>
2026-01-14 10:22:11 +0800

    修改默认端口为 80

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { createApp } = require('./src/app')
 const { startSchedulerIfEnabled } = require('./src/crawler/scheduler')
 
 const app = createApp()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 80
 
 app.listen(PORT, () => {
   console.log(`TenderRadar backend listening on http://localhost:${PORT}`)
